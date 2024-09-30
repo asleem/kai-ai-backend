a2965b93366722aa1e41646844ac70bacd7348ae
asleem <abir.sleem06@gmail.com>
2024-09-30 13:11:18 -0400

    added usit test

---
+++ app/features/ai_resistant_assignment_generator/tests/test_core.py
@@ -0,0 +1,36 @@
+import pytest
+from app.features.quizzify.core import executor
+# from app.api.error_utilities import SyllabusGeneratorError
+from app.services.schemas import AIResistantArgs
+
+def test_executor_pdf_url_valid():
+
+    ai_resistant_args = AIResistantArgs(
+        topic = "Introduction to Data Science",
+        assignment = "Develop a Neural Network Architecture for emulating Data Science Workflow",
+        grade_level = "college",
+        file_type = "pdf",
+        file_url = "http://ijsmsjournal.org/2021/volume-4%20issue-4/ijsms-v4i4p137.pdf",
+        lang = "en"
+    )
+    assignment = executor(ai_resistant_args)
+
+    assert isinstance(assignment, list)
+    assert len(assignment) == 3
+
+
+def test_executor_pdf_url_valid():
+
+    ai_resistant_args = AIResistantArgs(
+        topic = "Introduction to Data Science",
+        assignment = "Develop a Neural Network Architecture for emulating Data Science Workflow",
+        grade_level = "college",
+        file_type = "pdf",
+        file_url = "http://ijsmsjournal.org/2021/volume-4%20issue-4/dummy.pdf",
+        lang = "en"
+    )
+
+    with pytest.raises(ValueError) as exc_info:
+        assignment = executor(ai_resistant_args)
+
+    assert isinstance(exc_info.value, ValueError)
